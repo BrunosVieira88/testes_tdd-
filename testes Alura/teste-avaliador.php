f703184c3efaa1f2386f7612a0dad32917ba60f5
Bruno Vieira <brunosvieira88@gmail.com>
2024-05-05 21:10:19 -0300

    comentarios

--- testes Alura/teste-avaliador.php
+++ testes Alura/teste-avaliador.php
@@ -1,5 +1,8 @@
 <?php
 
+//Arrange act Assert 
+//Given when Then
+
 use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
 use Alura\Leilao\Model\Usuario;
@@ -7,6 +10,7 @@ use Alura\Leilao\Service\Avaliador;
 
 require 'vendor/autoload.php';
 
+//Given 
 $leilao = new Leilao('fiat 147 0km');
 $maria = new Usuario('Maria');
 $joao = new Usuario('Joao');
@@ -15,10 +19,11 @@ $joao = new Usuario('Joao');
 $leilao->recebeLance(new Lance($joao,2000));
 $leilao->recebeLance(new Lance($maria,2500));
 
-
+//When 
 $leiloeiro = new Avaliador();
 $leiloeiro->avalia($leilao);
 
 $vencedor =  $leiloeiro->getMaiorValor();
 
-echo $vencedor;
+//Then
+echo $vencedor == 2500 ? 'Testes ok' : 'Erro nos testes';
